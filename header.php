fa190f232b1e823571f2d4ed24da63e9ddc8bedb
Ajay <ajay.sheokand@meetribbon.com>
2022-11-15 19:07:58 +0530

    Spicy to Cloud

---
+++ header.php
@@ -0,0 +1,39 @@
+<header class="header-section">
+        <div class="container-fluid">
+            <div class="inner-header">
+                <div class="logo">
+                    <a href="./index.php"><img src="img/logo1.png" alt=""></a>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <nav class="main-menu mobile-menu">
+                                <ul>
+                                    <li><a href="./index.php">Home</a></li>
+                                    <li><a href="./about-us.php">About</a></li>
+                                    <!-- <li><a href="./rooms.php">Rooms</a></li> -->
+                                    <li><a href="#">Facilities</a>
+                                        <ul class="drop-menu">
+                                            <li><a href="#">Avilable Soon</a></li>
+                                            <!-- <li><a href="#">Double Room</a></li>
+                                            <li><a href="#">Senior Suit</a></li>
+                                            <li><a href="#">Single Room</a></li> -->
+                                        </ul>
+                                    </li>
+                                    
+                                    <li><a href="./contact.php">Contact</a></li>
+                                    <!-- <li><a href="spicyrasoi/user/createuser.php">Register</a></li> -->
+                                    <li><a href="spicyrasoi/user/login.php">Login</a></li>
+                                </ul>
+                            </nav>
+                            <div class="top-info">
+                                <img src="img/placeholder.png" alt="">
+                                <span>Narwana Distt Jind Haryana 126116</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="mobile-menu-wrap"></div>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Hotel Template">
+    <meta name="keywords" content="Hotel, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Spicy Rasoi</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Taviraj:300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/flaticon.css" type="text/css">
+    <link rel="stylesheet" href="css/linearicons.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Header Section Begin -->
+    <?php include("header.php"); ?>
+    <!-- Header End -->
+
+    <!-- Hero Slider Begin -->
+    <div class="hero-slider">
+        <div class="slider-item">
+            <div class="single-slider-item set-bg" data-setbg="img/slider-1.jpg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1>Hotel/Resturant Biling & Account's Managment</h1>
+                        </div>
+                    </div>
+                    <!-- <div class="row">
+                        <div class="col-lg-12">
+                            <div class="slider-nav">
+                                <a href="#" class="single-slider-nav">
+                                    <img src="img/nav-3.jpg" alt="">
+                                    <div class="nav-text active">
+                                        <p>Order Now<i class="lnr lnr-arrow-right"></i></p>
+                                    </div>
+                                </a>
+                                <a href="#" class="single-slider-nav">
+                                    <img src="img/nav-2.jpg" alt="">
+                                    <div class="nav-text">
+                                        <p>Open 24 * 7<i class="lnr lnr-arrow-right"></i></p>
+                                    </div>
+                                </a>
+                                <a href="#" class="single-slider-nav last">
+                                    <img src="img/nav-1.jpg" alt="">
+                                    <div class="nav-text">
+                                        <p>Restaurant<i class="lnr lnr-arrow-right"></i></p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Hero Slider End -->
+
+    <!-- Room Availability Section Begin -->
+    <section class="room-availability spad">
+        <div class="container">
+            <div class="room-check">
+                <div class="row">
+                    <!-- <div class="col-lg-6">
+                        <div class="room-item">
+                            <div class="room-pic-slider room-pic-item owl-carousel">
+                                <div class="room-pic">
+                                    <img src="img/room-footer-pic/2.jpg" alt="">
+                                </div>
+                                <div class="room-pic">
+                                    <img src="img/room-slider/room-2.jpg" alt="">
+                                </div>
+                                <div class="room-pic">
+                                    <img src="img/room-slider/room-3.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="room-text">
+                                <div class="room-title">
+                                    <h2>Your Fav. Food</h2>
+                                    <div class="room-price">
+                                        <span>From</span>
+                                        <h2>49 </h2>
+                                    </div>
+                                </div>
+                              
+                            </div>
+                        </div>
+                    </div> -->
+                    <div class="col-lg-12" >
+                    <div class="room-item">
+                            <div class="room-pic-slider room-pic-item owl-carousel" >
+                                <div class="room-pic">
+                                    <img src="img/room-footer-pic/21.jpg" alt="">
+                                </div>
+                                <div class="room-pic">
+                                    <img src="img/spicy_rasoi_pic/login.png" alt="">
+                                </div>
+                                <div class="room-pic">
+                                    <img src="img/room-slider/room-2.jpg" alt="">
+                                </div>
+                            </div>
+                            <!-- <div class="room-text">
+                                <div class="room-title">
+                                    <h2>Pizza</h2>
+                                    <div class="room-price">
+                                        <span>From</span>
+                                        <h2>49</h2>
+                                    </div>
+                                </div>
+                               
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="about-room">
+                <div class="row">
+                    <div class="col-lg-10 offset-lg-1">
+                        <h2>“Customers may forget what you said  but they will never forget how you made themfeel”.</h2>
+                    </div>
+                </div>
+                <div class="about-para">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus libero mauris,
+                                bibendum eget sapien ac, ultrices rhoncus ipsum. Donec nec sapien in urna fermentum
+                                ornare. Morbi vel ultrices leo. Sed eu turpis eu arcu vehicula fringilla ut vitae
+                                orci. Suspendisse maximus malesuada</p>
+                        </div>
+                        <div class="col-lg-6">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at vulputate est.
+                                Donec tempor felis at nibh eleifend malesuada. Nullam suscipit lobortis aliquam.
+                                Phasellus lobortis ante lorem, vitae scelerisque lacus tempus sed. Phasellus rutrum
+                                magna </p>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+        </div>
+    </section>
+    <!-- Room Availability Section End -->
+
+    <!-- Facilities Section Begin -->
+    <div class="facilities-section spad">
+        <div class="container">
+            <div class="facilities-content">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="section-title">
+                            <h1>Facilities</h1>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 p-0">
+                        <div class="facilities-img set-bg" data-setbg="img/room-footer-pic/1.png"></div>
+                    </div>
+                    <div class="col-lg-6 p-0 ">
+                        <div class="facilities-text-warp">
+                            <div class="facilities-text">
+                                <h2>Best Service</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipis-cing elit. Mauris tincidunt consectetur
+                                    turpis, eget consequat.</p>
+                                <a href="#" class="primary-btn fac-btn">Visit Center <i class="lnr lnr-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 p-0 order-lg-1 order-2">
+                        <div class="facilities-text-warp">
+                            <div class="facilities-text">
+                                <h2>Easy & Fast</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipis-cing elit. Mauris tincidunt consectetur
+                                    turpis, eget consequat.</p>
+                                <a href="#" class="primary-btn fac-btn">Visit Center <i class="lnr lnr-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 p-0 order-lg-2 order-1">
+                        <div class="facilities-img set-bg" data-setbg="img/room-footer-pic/3.jpg"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Facilities Section End -->
+<!-- 
+    <div class="testimonial-section spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h1>Guestbook</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="testimonial-item">
+                        <div class="tab-content">
+                            <div class="tab-pane fade-in active" id="testimonial-1" role="tabpanel">
+                                <div class="single-testimonial-item">
+                                    <span class="test-date">04/10/2021</span>
+                                    <div class="test-rating">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                    </div>
+                                    <h4>Loved It</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiselit. Vivamus libero mauris,
+                                        bibendum eget sapien ac, ultrices rhoncus ipsum. Donec nec sapien in urna
+                                        fermentum ornare.</p>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="testimonial-2" role="tabpanel">
+                                <div class="single-testimonial-item">
+                                    <span class="test-date">01/10/2021</span>
+                                    <div class="test-rating">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                    </div>
+                                    <h4>Loved It2</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiselit. Vivamus libero mauris,
+                                        bibendum eget sapien ac, ultrices rhoncus ipsum. Donec nec sapien in urna
+                                        fermentum ornare.</p>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="testimonial-3" role="tabpanel">
+                                <div class="single-testimonial-item">
+                                    <span class="test-date">02/10/2021</span>
+                                    <div class="test-rating">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                    </div>
+                                    <h4>Loved It3</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiselit. Vivamus libero mauris,
+                                        bibendum eget sapien ac, ultrices rhoncus ipsum. Donec nec sapien in urna
+                                        fermentum ornare.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="testimonial-author-item">
+                        <ul class="nav" role="tablist">
+                            <li>
+                                <a data-toggle="tab" href="#testimonial-1" role="tab" class="show active">
+                                    <div class="author-pic">
+                                        <img src="img/testimonial/author-1.jpg" alt="">
+                                    </div>
+                                    <div class="author-text">
+                                        <h5>Sudhir <span>Narwana</span></h5>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a data-toggle="tab" href="#testimonial-2" role="tab">
+                                    <div class="author-pic">
+                                        <img src="img/testimonial/author-2.jpg" alt="">
+                                    </div>
+                                    <div class="author-text">
+                                        <h5>Angel <span>Jind</span></h5>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a data-toggle="tab" href="#testimonial-3" role="tab">
+                                    <div class="author-pic">
+                                        <img src="img/testimonial/author-1.jpg" alt="">
+                                    </div>
+                                    <div class="author-text">
+                                        <h5>Abhishek <span>Jind</span></h5>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+
+    <!-- Follow Instagram Section Begin -->
+    <section class="follow-instagram">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2>Follow us on Instagram @ajay.sheokand</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Follow Instagram Section End -->
+
+    <!-- Footer Room Pic Section Begin -->
+    <div class="footer-room-pic">
+        <div class="container-fluid">
+            <div class="row">
+                <img src="img/room-footer-pic/21.jpg" alt="">
+                <img src="img/room-footer-pic/3.jpg" alt="">
+                <img src="img/room-footer-pic/4.jpg" alt="">
+                <img src="img/room-footer-pic/6.jpg" alt="">
+            </div>
+        </div>
+    </div>
+    <!-- Footer Room Pic Section End -->
+
+    <?php include("footer.php"); ?>
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
